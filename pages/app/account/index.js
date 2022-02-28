d9142d87af8056176eac7be525d403b171e5f87d
Uba Noel <ubanoel2000@gmail.com>
2022-02-28 17:48:50 +0100

    update

--- pages/app/account/index.js
+++ pages/app/account/index.js
@@ -100,7 +100,7 @@ function index() {
                 value={selectedDateValue}
                 max={disabledDates}
               />
-              <span className='absolute right-4 top-[12px] cursor-pointer'>
+              <span className='absolute right-4 top-[12px] cursor-pointer hidden'>
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
                 </svg>
