46e9796716feb1991b243451303d9881f5dbe1ed
uba2000 <ubanoel2000@gmail.com>
2022-03-07 20:11:41 +0100

    add edit functionality

--- components/app/article/GeneratorMainBody.js
+++ components/app/article/GeneratorMainBody.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react'
 import dynamic from "next/dynamic"
 
 import { PencilWhite, Pencil, Wordpress, Docs } from '../../../ui/icons'
+import styles from '../../../styles/Account.module.css'
 import Refresh from '../../Refresh'
 
 export class GeneratorMainBody extends Component {
@@ -10,8 +11,10 @@ export class GeneratorMainBody extends Component {
     super(props)
 
     this.state = {
+      title: 'What Is E-Commerce SEO?',
       checked: false,
       showEditor: false,
+      showEditTitle: false,
     }
   }
 
@@ -19,6 +22,14 @@ export class GeneratorMainBody extends Component {
     this.setState((prevState) => {
       return {
         showEditor: !prevState.showEditor
+      }
+    })
+  }
+
+  editTitleHandler = () => {
+    this.setState((prevState) => {
+      return {
+        showEditTitle: !prevState.showEditTitle
       }
     })
   }
@@ -46,25 +57,39 @@ export class GeneratorMainBody extends Component {
     return (
       <div className="generator-box bg-white">
         <header className='generator-box-header-box flex justify-between py-[13px] px-[25px]'>
-          <p className="relative text-base leading-7 font-poppins font-bold capitalize flex h-[27.99px]">
+          {!this.state.showEditTitle ? (<><p className="relative text-base leading-7 font-poppins font-bold capitalize flex h-[27.99px]">
             <span className='text-primary mr-1'>H1:</span>
-            <span className='flex-grow flex-shrink line-clamp-1'>What Is E-Commerce SEO?{' '}</span>
-            <span className='cursor-pointer block absolute -top-2 right-[-2px]'>
+            <span className='flex-grow flex-shrink line-clamp-1'>{this.state.title}{' '}</span>
+            <span onClick={this.editTitleHandler} className='cursor-pointer block absolute -top-2 right-[-2px]'>
               <Pencil />
             </span>
           </p>
-          <p className='whitespace-nowrap'>
-            <span>{articleContent.split(' ').length}</span> words
-          </p>
+            <p className='whitespace-nowrap'>
+              <span>{articleContent.split(' ').length}</span> words
+            </p></>) : (
+            <>
+              {/*  */}
+              <p className="items-center relative text-base leading-7 font-poppins font-bold capitalize flex flex-grow flex-shrink">
+                <span className='text-primary mr-1'>H1:</span>
+                <input type="text" value={this.state.title} onChange={(e) => this.setState({ title: e.target.value })} className={styles.formGroupInput} style={{ maxWidth: '384px', width: '100%', height: '35px' }} />
+              </p>
+              <div className="">
+                <button className="btn btn-reset py-[9px] px-[20px]">Reset</button>
+                <button className="btn btn-primary py-[9px] px-[20px]" onClick={this.editTitleHandler}>Save</button>
+              </div>
+            </>
+          )}
         </header>
         <div className="generator-body-section py-[15px] px-[25px]">
-          <p className="content line-clamp-6 block text-base leading-8 font-poppins">
+          {!this.state.showEditor ? (<p className="content line-clamp-6 block text-base leading-8 font-poppins">
             {articleContent}
-          </p>
+          </p>) : (
+            <ArticleEditor content={articleContent} />
+          )}
         </div>
         <footer className='border-t border-solid border-[#dcd8e7] py-[18.5px] px-[25px]'>
           <div className="flex space-x-[10px] justify-end">
-            <button className='justify-center items-center cursor-pointer flex bg-primary border border-[#DCD8E7] border-solid btn py-2 px-[13px]'>
+            {!this.state.showEditor ? (<><button onClick={this.showEditorHandler} className='justify-center items-center cursor-pointer flex bg-primary border border-[#DCD8E7] border-solid btn py-2 px-[13px]'>
               <span className='mr-[9px]'>
                 <PencilWhite />
               </span>
@@ -72,14 +97,19 @@ export class GeneratorMainBody extends Component {
                 Edit
               </span>
             </button>
-            <button className='justify-center items-center cursor-pointer flex bg-white border border-[#DCD8E7] border-solid btn py-2 px-[13px]'>
-              <span className='mr-[9px]'>
-                <Refresh className="w-[22px] h-[15px]" />
-              </span>
-              <span className='text-[12px] leading-[120%] font-semibold font-poppins'>
-                Refresh
-              </span>
-            </button>
+              <button className='justify-center items-center cursor-pointer flex bg-white border border-[#DCD8E7] border-solid btn py-2 px-[13px]'>
+                <span className='mr-[9px]'>
+                  <Refresh className="w-[22px] h-[15px]" />
+                </span>
+                <span className='text-[12px] leading-[120%] font-semibold font-poppins'>
+                  Refresh
+                </span>
+              </button></>) : (
+              <>
+                <button className="btn btn-reset py-[9px] px-[20px]">Reset</button>
+                <button className="btn btn-primary py-[9px] px-[20px]" onClick={this.showEditorHandler}>Save</button>
+              </>
+            )}
           </div>
         </footer>
       </div>
