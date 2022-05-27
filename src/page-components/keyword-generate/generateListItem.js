4e49ba178e9248e7de049bf35840439f961a049b
uba2000 <ubanoel2000@gmail.com>
2022-05-27 16:54:59 +0100

    updates

--- src/page-components/keyword-generate/generateListItem.js
+++ src/page-components/keyword-generate/generateListItem.js
@@ -115,7 +115,11 @@ const TableStatus = ({ status }) => {
           </span>
         ) : status == 'waiting' && (
           <span>
-            <Waiting {...dim} />
+            <svg {...dim} viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <rect width="16" height="16" rx="8" className='dark:fill-black fill-white' />
+              <path d="M8 15.0029C4.13401 15.0029 1 11.8689 1 8.00293C1 4.13694 4.13401 1.00293 8 1.00293C11.866 1.00293 15 4.13694 15 8.00293C15 11.8689 11.866 15.0029 8 15.0029Z" className='dark:stroke-white stroke-black' strokeWidth="1.5" stroke-linecap="round" stroke-linejoin="round" />
+              <path d="M6 11L7.60933 9.3102C7.8594 9.04771 7.99992 8.69167 8 8.3204V4" className='dark:stroke-white stroke-black' strokeWidth="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
           </span>
         )}
         <span className='capitalize'>{status}</span>
