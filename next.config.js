f6f76460eb60254ab4fb33429f42571f4f1d3056
uba2000 <ubanoel2000@gmail.com>
2022-05-31 13:32:41 +0100

    update base URL

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    BASE_URL: 'http://localhost:3000',
+    BASE_URL: `${process.env.NODE_ENV == 'development' ? 'http://localhost:3000' : 'https://next-dashboard-design.vercel.app'}`,
     OPEN_API: 'https://scai-rhasi6a2qa-uc.a.run.app',
   },
 }
