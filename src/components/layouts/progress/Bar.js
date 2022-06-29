66a43e8607715c97c4f03fe006f52f6c7ad04e4f
uba2000 <ubanoel2000@gmail.com>
2022-06-29 11:12:31 +0100

    update loading bar color

--- src/components/layouts/progress/Bar.js
+++ src/components/layouts/progress/Bar.js
@@ -1,6 +1,6 @@
 export const Bar = ({ animationDuration, progress }) => (
   <div
-    className="bg-primary h-1 w-full left-0 top-0 fixed z-50"
+    className="bg-gradient-to-r from-[rgb(211,248,154)] via-[rgb(127,251,169)] to-[rgb(64,237,195)] h-1 w-full left-0 top-0 fixed z-50"
     style={{
       marginLeft: `${(-1 + progress) * 100}%`,
       transition: `margin-left ${animationDuration}ms linear`,
