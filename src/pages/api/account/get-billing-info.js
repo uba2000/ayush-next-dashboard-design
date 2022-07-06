9c33ff834eecbb40e2042e84232c98dc2c18cc80
uba2000 <ubanoel2000@gmail.com>
2022-07-06 07:23:21 +0100

    fix payment error bug

--- src/pages/api/account/get-billing-info.js
+++ src/pages/api/account/get-billing-info.js
@@ -29,36 +29,12 @@ export default async function (req, res) {
           'payment_methods current_plan'
         );
 
-        let paymentMethods = [];
-
-        forEach(userPaymentDetails.payment_methods, (value, index) => {
-          paymentMethods.push({
-            ...value._doc,
-            card_number: Crypto.AES.decrypt(
-              value.card_number,
-              process.env.ENC_SECRET
-            ).toString(Crypto.enc.Utf8),
-            mask_card_number: cardHide(
-              Crypto.AES.decrypt(
-                value.card_number,
-                process.env.ENC_SECRET
-              ).toString(Crypto.enc.Utf8)
-            ),
-            exp_date: Crypto.AES.decrypt(
-              value.exp_date,
-              process.env.ENC_SECRET
-            ).toString(Crypto.enc.Utf8),
-            security_code: Crypto.AES.decrypt(
-              value.security_code,
-              process.env.ENC_SECRET
-            ).toString(Crypto.enc.Utf8),
-          });
-        });
+        let paymentMethods = userPaymentDetails.payment_methods;
 
         return res.status(200).json({
           success: true,
           data: {
-            paymentMethods,
+            paymentMethods: [],
             currentPlan: userPaymentDetails.current_plan,
             intent: {
               client_secret: setupIntent.client_secret,
