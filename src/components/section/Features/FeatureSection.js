199561e110c3ab893a5110abb2e52b00261739db
uba2000 <ubanoel2000@gmail.com>
2022-08-12 15:23:28 +0100

    chore: update dashboard landing layout

--- src/components/section/Features/FeatureSection.js
+++ src/components/section/Features/FeatureSection.js
@@ -5,7 +5,7 @@ import FeatureBox from './FeatureBox';
 const FeatureSection = ({ features }) => {
   return (
     <div className="min-h-[600px]">
-      <div className="grid md:grid-cols-3 lg:grid-cols-4 grid-cols-1 gap-5">
+      <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 grid-cols-1 gap-5">
         {features.map((feature) => {
           return (
             <Fragment key={feature._id}>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
-const colors = require('tailwindcss/colors')
-
+const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   important: true,
   darkMode: 'class',
-  plugins: [
-    require('@tailwindcss/line-clamp'),
-  ],
+  plugins: [require('@tailwindcss/line-clamp')],
   content: [
     './src/public/**/*.html',
     './src/pages/**/*.{js,jsx,ts,tsx,vue}',
@@ -16,7 +14,12 @@ module.exports = {
   theme: {
     container: {
       center: true,
-      padding: '15px',
+      padding: '18px',
+    },
+    screens: {
+      // mobile: { max: '425px' },
+      // mlg: '992px',
+      ...defaultTheme.screens,
     },
     extend: {
       minHeight: (theme) => ({
@@ -26,31 +29,31 @@ module.exports = {
         ...theme('spacing'),
       }),
       minWidth: {
-        'screen': '100vw',
+        screen: '100vw',
         'card-lg': '16rem',
-        'card': '14rem',
+        card: '14rem',
       },
       maxWidth: {
         '8xl': '1600px',
-        '1/3': '33.333333%'
+        '1/3': '33.333333%',
       },
       fontFamily: {
-        poppins: ['Poppins', 'sans-serif',],
-        inter: ['Inter',],
-        helvetica: ['Helvetica',],
-        roboto: ['Roboto', 'sans-serif',],
+        poppins: ['Poppins', 'sans-serif'],
+        inter: ['Inter'],
+        helvetica: ['Helvetica'],
+        roboto: ['Roboto', 'sans-serif'],
       },
       colors: {
-        'buttonGreen': '#00A141',
+        buttonGreen: '#00A141',
         'primary-100': 'rgba(0, 161, 65, 0.1)',
         'primary-300': 'rgba(0, 161, 65, 0.3)',
         'primary-700': 'rgba(0, 161, 65, 0.7)',
-        'primary': '#00A141',
+        primary: '#00A141',
         'primary-light': '#F8FFFB',
-        'green': 'rgb(64, 237, 195)',
+        green: 'rgb(64, 237, 195)',
         'green-500': 'rgb(127, 251, 169)',
-        'yellow': 'rgb(211, 248, 154)',
-        'ash': '#757575',
+        yellow: 'rgb(211, 248, 154)',
+        ash: '#757575',
         'gray-500': '#44444F',
         'gray-800': '#dcd8e7',
         'gray-900': '#E2E2EA',
@@ -59,29 +62,47 @@ module.exports = {
         'darkMode-border': '#414141',
         'darkMode-black': '#000000',
         'darkMode-bg': '#111111',
-        'purple': colors.violet,
-        'red': '#FF3749',
+        purple: colors.violet,
+        red: '#FF3749',
       },
       fontSize: {
-        'title': ['33.7069px', {
-          lineHeight: '50px'
-        }],
-        'subtitle': ['12.0865px', {
-          lineHeight: '14px'
-        }],
-        'brand': ['35px', {
-          lineHeight: '52px'
-        }],
-        'landing': ['50px', {
-          lineHeight: '47px'
-        }],
-        'subLanding': ['18px', {
-          lineHeight: '27px'
-        }],
-        'wild': ['19.1978px', {
-          lineHeight: '135%'
-        }]
-      }
-    }
+        title: [
+          '33.7069px',
+          {
+            lineHeight: '50px',
+          },
+        ],
+        subtitle: [
+          '12.0865px',
+          {
+            lineHeight: '14px',
+          },
+        ],
+        brand: [
+          '35px',
+          {
+            lineHeight: '52px',
+          },
+        ],
+        landing: [
+          '50px',
+          {
+            lineHeight: '47px',
+          },
+        ],
+        subLanding: [
+          '18px',
+          {
+            lineHeight: '27px',
+          },
+        ],
+        wild: [
+          '19.1978px',
+          {
+            lineHeight: '135%',
+          },
+        ],
+      },
+    },
   },
-}
+};
